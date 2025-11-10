a0dfc5c8967e1e93efc6bca780d98a2af35e63c5
hperezrodal <hperezrodal@gmail.com>
2025-11-10 12:59:53 -0300

    feat: update eks module update cluster version

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.30"
+  default     = "1.34"
 }
 
 variable "vpc_id" {
